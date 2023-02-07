15b926f1a3929a74bf465edca237251a61289ab2
Sai Abhishek Kalakkadu Bhaskaran <sakalakk@uwaterloo.ca>
2023-02-06 22:01:57 -0500

    Okta OIDC JWT verification

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# reactive-rest-spring-boot
+# Reactive RESTful Web Service
+
+Self learning project - a reactive programming model to construct REST APIs performing non-blocking CRUD operations on MongoDB backend
+
+REST APIs are secured with OpenID Connect authentication - validated against JWT token in request header
+
+## Tech stack
+* Spring Webflux
+* Spring Data Reactive MongoDB
+* Okta OIDC with Spring Security
+
+
+
---
+++ src/main/java/com/reactiverest/studentapp/config/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.reactiverest.studentapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    // Intercepts all incoming requests by verifying JWT token
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+}
